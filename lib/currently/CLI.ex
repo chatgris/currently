c09d73e4a3d7926eafbb4f5171876c5a9462a00f
chatgris <jboyer@af83.com>
2013-07-22 09:07:01 +0200

    Add Currently.CLI
    Make use of doctest to generate tests.
    
    Signed-off-by: chatgris <jboyer@af83.com>

---
+++ lib/currently/CLI.ex
@@ -0,0 +1,53 @@
+defmodule Currently.CLI do
+  @module_doc """
+
+  Display cards currently assigns to the authenticated user.
+  """
+
+  def run(argv) do
+    argv
+      |> parse_args
+      |> process
+  end
+
+  @doc """
+  `argv` can be -h or --help, which returns :help.
+
+  Otherwise it is a token provides by trello.
+
+  Return a tuple of `{token}`, or `:help` if help was given.
+
+  ## Examples
+
+      iex> Currently.CLI.parse_args(["-h"])
+      :help
+
+      iex> Currently.CLI.parse_args(["-help", "token"])
+      :help
+
+      iex> Currently.CLI.parse_args(["token"])
+      {"token"}
+
+  """
+  def parse_args(argv) do
+    parse = OptionParser.parse(argv, switches: [help: :boolean],
+                                     aliases: [h: :help]
+    )
+    case parse do
+      {[help: true], _}   -> :help
+      {_, [token]}        -> {token}
+      _                   -> :help
+    end
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage: currently <token>
+    """
+    System.halt(0)
+  end
+
+  def process(token) do
+    IO.puts token
+  end
+end
