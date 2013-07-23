66c8673ff32bcb07c6f6ab20842226084a776170
chatgris <jboyer@af83.com>
2013-07-23 09:11:20 +0200

    Fetch data from Trello
    Signed-off-by: chatgris <jboyer@af83.com>

--- lib/currently/cli.ex
+++ lib/currently/cli.ex
@@ -25,8 +25,8 @@ defmodule Currently.CLI do
       iex> Currently.CLI.parse_args(["-help", "token"])
       :help
 
-      iex> Currently.CLI.parse_args(["token"])
-      {"token"}
+      iex> Currently.CLI.parse_args(["key", "token"])
+      {"key", "token"}
 
   """
   def parse_args(argv) do
@@ -35,19 +35,39 @@ defmodule Currently.CLI do
     )
     case parse do
       {[help: true], _}   -> :help
-      {_, [token]}        -> {token}
+      {_, [key, token]}   -> {key, token}
       _                   -> :help
     end
   end
 
   def process(:help) do
     IO.puts """
-    usage: currently <token>
+    usage: currently <key> <token>
     """
     System.halt(0)
   end
 
-  def process(token) do
-    IO.puts token
+  def process(key, token) do
+    Currently.TrelloCards.fetch(key, token)
+      |> decode_response
+      |> display_cards(["name", "due", "shortUrl"])
+  end
+
+  def decode_response({:ok, body}) do
+    Jsonex.decode(body)
+  end
+
+  def decode_response({:error, msg}) do
+    IO.puts "Error fetching from trello: #{msg}"
+    System.halt(2)
+  end
+
+  def display_cards(cards, fields) do
+    Enum.each cards, display_card(&1, fields)
+  end
+
+  def display_card(card, fields) do
+    IO.puts Enum.map(fields, fn field -> card[field] end)
+      |> Enum.join " ,"
   end
 end
---
+++ lib/currently/trello_cards.ex
@@ -0,0 +1,19 @@
+defmodule Currently.TrelloCards do
+  alias HTTPotion.Response
+
+  @user_agent ["User-agent": "Trello cards"]
+
+  def fetch(key, token) do
+    case HTTPotion.get(issues_url(key, token), @user_agent) do
+      Response[body: body, status_code: status, headers: _headers]
+      when status in 200..299 ->
+        {:ok, body}
+      Response[body: body, status_code: _status, headers: _headers] ->
+        {:error, body}
+    end
+  end
+
+  def issues_url(key, token) do
+    "https://api.trello.com/1/members/my/cards?token=#{token}&key=#{key}"
+  end
+end
