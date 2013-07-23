c0178beca1428a34ccd7f6359458b22cc0040e39
chatgris <jboyer@af83.com>
2013-07-23 08:33:36 +0200

    Add httpotion and jsonex
    Signed-off-by: chatgris <jboyer@af83.com>

--- mix.exs
+++ mix.exs
@@ -16,6 +16,9 @@ defmodule Currently.Mixfile do
   # Returns the list of dependencies in the format:
   # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    []
+    [
+      {:httpotion, "0.2.0", [github: "myfreeweb/httpotion"]},
+      {:jsonex,    "2.0",   [github: "marcelog/jsonex", tag: "2.0"]}
+    ]
   end
 end
