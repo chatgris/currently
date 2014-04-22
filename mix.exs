ef7076f326cfa3189830aa97e4563be853a600a0
chatgris <jboyer@af83.com>
2014-04-22 10:39:54 +0200

    Require Elixir v0.13
    Signed-off-by: chatgris <jboyer@af83.com>

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Currently.Mixfile do
   def project do
     [ app: :currently,
       version: "0.0.1",
-      elixir: "~> 0.12",
+      elixir: "~> 0.13",
       escript_main_module: Currently,
       escript_embed_elixir: true,
       deps: deps ]
